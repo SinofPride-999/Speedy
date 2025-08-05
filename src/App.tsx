d19dd1c02dda32d0562c3edd33d77083c87b4438
SinofPride-999 <jhaycodes999@gmail.com>
2025-08-05 11:38:42 +0000

    update: added shortcut-hint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import SearchBar from './components/SearchBar/SearchBar';
 import './App.css';
 
 const App: React.FC = () => {
-  return <SearchBar />;
+  return (
+    <>
+      <SearchBar />
+      <div className="shortcut-hint">
+        Press <kbd>⌘</kbd> + <kbd>Space</kbd> on Mac or <kbd>Ctrl</kbd> + <kbd>Space</kbd> on Windows
+      </div>
+    </>
+  );
 };
 
 export default App;
